19d0f7f8bd5d96f28684dbdec4e8bea391f29c70
Johann Wagner <johann@movidone.com>
2017-11-10 13:54:59 +0100

    v3.0.1 for Prestashop v1.7

--- easytransac.php
+++ easytransac.php
@@ -18,7 +18,7 @@ class EasyTransac extends PaymentModule
     {
         $this->name = 'easytransac';
         $this->tab = 'payments_gateways';
-        $this->version = '3.0.0';
+        $this->version = '3.0.1';
         $this->author = 'EasyTransac';
         $this->is_eu_compatible = 1;
         $this->need_instance = 0;
